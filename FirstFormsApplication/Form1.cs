b1547b663718639d3c4c7dcbdc1af6140fb4ed49
Erika Yardumian <eyardumian@hotmail.com>
2019-08-23 15:12:12 -0700

    User prompt added

--- FirstFormsApplication/Form1.cs
+++ FirstFormsApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace FirstFormsApplication
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

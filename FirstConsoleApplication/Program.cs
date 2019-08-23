29f54254e23a49be84a9c8bb5a8f8973bf4c3746
Erika Yardumian <eyardumian@hotmail.com>
2019-08-22 18:39:09 -0700

    First number conversion

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -11,8 +11,8 @@ namespace FirstConsoleApplication
         static void Main(string[] args)
         {
             Console.Write("Enter a whole number.");
-         
-       
+            int FirstNum = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
